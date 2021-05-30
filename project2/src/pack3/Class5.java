8083f76888b8a013bdd11d805e96d28d04e643d1
Gonapati <vinodreddy0032@gmail.com>
2021-05-30 06:35:02 +0530

    added class 5 into project 2

---
+++ project2/src/pack3/Class5.java
@@ -0,0 +1,13 @@
+package pack3;
+
+public class Class5 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("third message");
+		System.out.println("fourth message");
+
+	}
+
+}
