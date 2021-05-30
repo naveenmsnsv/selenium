abf29b8e3f16f4b91ad6c4d9425dcbff262efd46
Gonapati <vinodreddy0032@gmail.com>
2021-05-30 07:08:51 +0530

    added class 7 and messages in class 6 and class 7

--- project2/src/pack3/Class6.java
+++ project2/src/pack3/Class6.java
@@ -5,7 +5,7 @@ public class Class6 {
 	public static void main(String[] args) 
 	{
 		System.out.println("fifth message");
-
+		System.out.println("seventh message");
 	}
 
 }
---
+++ project2/src/pack3/Class7.java
@@ -0,0 +1,10 @@
+package pack3;
+
+public class Class7 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("eighth message");
+	}
+
+}
